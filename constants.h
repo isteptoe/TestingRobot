ea811edeca40e7c64521e2124064c43119a2a7a6
Ian Steptoe <steptoeianb@gmail.com>
2018-07-03 00:09:04 -0400

    Initial commit

---
+++ constants.h
@@ -0,0 +1,51 @@
+#ifndef CONSTANTS
+
+#define CONSTANTS
+
+//sorter constants
+const int ORANGE = 135;
+const int PICK_UP = 98;
+const int WHITE = 65;
+const int BALL_TRIGGER = 5000;
+const int SORT_TIME = 50;
+
+//speed constants
+const int SLOW_SPEED = 50;
+const int HALF_SPEED = 100;
+const int FULL_SPEED = 150;
+
+//Joel constants
+const int TARGET_INDEX = 3;
+const int TURN_AMOUNT  = 3;
+const char LEFT = 'l';
+const char RIGHT = 'r';
+const char FORWARD = 'f';
+const char BACK = 'b';
+
+// Dasch constants
+const int  BLUETOOTH_LIMITER = 4000;
+const bool WHEEL_FORWARDS   = true;
+const bool WHEEL_BACKWARDS  = false;
+const bool FORWARDS         = true;
+const bool BACKWARDS        = false;
+
+
+const int DO_DUMP = 95;
+const int DONT_DUMP = 200;
+
+
+const int ARM_RIGHT = 150;
+const int ARM_MIDDLE = 100;
+const int ARM_LEFT = 50;
+// Paths
+
+const char RED_MISSION_1[] = { FORWARD, FORWARD, FORWARD, RIGHT, FORWARD, FORWARD, LEFT, LEFT, BACK, BACK, BACK, BACK };
+const char NEUTRAL_MISSION_1[] = { FORWARD, FORWARD, FORWARD, FORWARD, FORWARD, FORWARD, RIGHT, RIGHT, RIGHT, LEFT, LEFT, BACK, BACK, BACK, BACK, BACK, BACK, RIGHT };
+const char RED_PICKUP_1[] = { FORWARD, FORWARD, FORWARD, RIGHT, FORWARD, FORWARD, LEFT, LEFT, BACK, BACK, BACK, BACK };
+const char NEUTRAL_PICKUP_1[] = { FORWARD, FORWARD, FORWARD, FORWARD, FORWARD, FORWARD, RIGHT, RIGHT, RIGHT, LEFT, LEFT, BACK, BACK, BACK, BACK, BACK, BACK, RIGHT };
+
+
+
+
+
+#endif
